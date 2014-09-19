2d1906ba2ea378c81b4765b7cdab97a7c419add7
Riki Fridrich <riki@fczbkk.com>
2014-09-19 04:14:11 +0200

    added support for archaic versions of IE for `simulateEvent()`

--- helpers/simulate-event.js
+++ helpers/simulate-event.js
@@ -2,7 +2,24 @@ var simulateEvent;
 
 simulateEvent = function(obj, type) {
   var event;
-  event = document.createEvent('HTMLEvents');
-  event.initEvent(type, true, true);
-  return obj.dispatchEvent(event);
+  if (document.createEvent) {
+    event = document.createEvent('HTMLEvents');
+    event.initEvent(type, true, true);
+  } else if (document.createEventObject) {
+    event = document.createEventObject();
+    event.eventType = type;
+    event.eventName = type;
+  }
+  if (obj.dispatchEvent) {
+    obj.dispatchEvent(event);
+  }
+  if (obj.dispatchEvent) {
+    return obj.dispatchEvent(event);
+  } else if (obj.fireEvent && (typeof htmlEvents !== "undefined" && htmlEvents !== null ? htmlEvents["on" + type] : void 0)) {
+    return obj.fireEvent("on" + type, event);
+  } else if (obj[type]) {
+    return obj[type]();
+  } else if (obj["on" + type]) {
+    return obj["on" + type]();
+  }
 };
--- src/coffee/simulate-event.coffee
+++ src/coffee/simulate-event.coffee
@@ -1,4 +1,34 @@
 simulateEvent = (obj, type) ->
-  event = document.createEvent 'HTMLEvents'
-  event.initEvent type, true, true
-  obj.dispatchEvent event
+
+  # SETUP EVENT
+
+  # modern browsers
+  if document.createEvent
+    event = document.createEvent 'HTMLEvents'
+    event.initEvent type, true, true
+
+  # IE9-
+  else if document.createEventObject
+    event = document.createEventObject()
+    event.eventType = type
+    event.eventName = type
+
+
+  # FIRE EVENT
+
+  # modern browsers
+  if obj.dispatchEvent
+    obj.dispatchEvent event
+
+  # IE9-
+  if obj.dispatchEvent
+    obj.dispatchEvent event
+
+  else if obj.fireEvent and htmlEvents?["on#{type}"]
+    obj.fireEvent "on#{type}", event
+
+  else if obj[type]
+    obj[type]()
+
+  else if obj["on#{type}"]
+    obj["on#{type}"]()
