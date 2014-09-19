6afb1b62d38ddd973e4c2b1cd9384006ab8d6b19
Riki Fridrich <riki@fczbkk.com>
2014-09-19 04:36:34 +0200

    Release v0.5.2

--- helpers/get-css-property.js
+++ helpers/get-css-property.js
@@ -2,6 +2,15 @@ var getCssProperty;
 
 getCssProperty = function(elm, property) {
   var style;
-  style = window.getComputedStyle(elm, null);
-  return style.getPropertyValue(property);
+  if (window.getComputedStyle != null) {
+    style = window.getComputedStyle(elm, null);
+    return style.getPropertyValue(property);
+  }
+  if (elm.currentStyle != null) {
+    property = property.replace(/-(.)/g, function(match, group1) {
+      return group1.toUpperCase();
+    });
+    return elm.currentStyle[property];
+  }
+  return null;
 };
--- src/coffee/get-css-property.coffee
+++ src/coffee/get-css-property.coffee
@@ -1,3 +1,15 @@
 getCssProperty = (elm, property) ->
-  style = window.getComputedStyle elm, null
-  style.getPropertyValue property
+
+  # modern browsers
+  if window.getComputedStyle?
+    style = window.getComputedStyle elm, null
+    return style.getPropertyValue property
+
+  # old versions of IE
+  if elm.currentStyle?
+    # convert property name to camelCase
+    property = property.replace /-(.)/g, (match, group1) ->
+      group1.toUpperCase()
+    return elm.currentStyle[property]
+
+  null
